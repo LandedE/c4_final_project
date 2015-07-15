37131fd15cd7375f883eab3a594ca6fe45242c97
Evan Lanning <ewlanning@gmail.com>
2015-07-15 14:22:53 -0700

    changed getPlans() to text

--- jsindex.js
+++ jsindex.js
@@ -272,7 +272,7 @@ function createDescriptionText(eventData){
 				};
     				$.ajax({
     					url: 'gen_plan.php',
-    					dataType: 'json',
+    					dataType: 'text',
     					data: {
     						0: $('#search_param1').val(),
     						1: $('#search_param2').val(),
